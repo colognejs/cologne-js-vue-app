c559463087944710bed6ac184529f75fa06c73b3
Joshua Blum <hallo@joshuablum.de>
2022-02-09 13:56:10 +0100

    Installed Tailwind CSS and packages from its ecosystem

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import postcss from "./postcss.config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  css: {
+   postcss,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
